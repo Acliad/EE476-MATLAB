5b89d2f936b3d5ff1e9045dbed8f745758c1360d
Isaac <irex@iastate.edu>
2020-03-19 07:33:48 -0500

    Cleaned up code

--- HW3/EE476_hw3.m
+++ HW3/EE476_hw3.m
@@ -2,10 +2,17 @@
 data = readtable("stars.txt");
 % Make the column headers look nice
 data.Properties.VariableNames = {'ID', 'R', 'i', 'Omega', 'phi', 'theta'};
+% Convert degrees to radians
 data.i = deg2rad(data.i);
 data.Omega = deg2rad(data.Omega);
 data.phi = deg2rad(data.phi);
 data.theta = deg2rad(data.theta);
+
+% Constants and options
+MAX_STARS = length(data.ID);
+REC_VIDEO = false;
+
+
 % Using the initial data, set up the initial state for the state variables
 % equations utilized from the second part of instructions, t=0
 % x1 = x position, x2 = x velocity, x3 = y position, x4 = y velocity
@@ -29,21 +36,20 @@ initial = table(...
     orbit_speed(data.R) .* (cos(data.phi) .* sin(data.i)), ...
     'VariableNames', {'x1i', 'x2i' , 'x3i', 'x4i', 'x5i', 'x6i'});
 
-% x, dx/dt, y, dy/dt, z, dz/dt
-ss_initial = [-1.12,-211.48,-3.10,129.12,-3.40,-48.23];
 % time (Myrs)
 t = linspace(0, 90, 5000);
-% For debugging: planet ID
-id = 5;
 
 % **********
 % * Solve space ship motion
 % **********
+
+% x, dx/dt, y, dy/dt, z, dz/dt
+ss_initial = [-1.12,-211.48,-3.10,129.12,-3.40,-48.23];
+
 [ss_t, ss_sol] = ode23(@(t, x) ssmodel(x), t, ss_initial);
 ss_x = ss_sol(:, 1);
 ss_y = ss_sol(:, 3);
 ss_z = ss_sol(:, 5);
-
 
 % **********
 % * Solve stars' motion
@@ -57,15 +63,6 @@ y = data.R .* (cos(n.*t + data.phi) .* sin(data.Omega) + ...
     sin(n.*t + data.phi) .* cos(data.i) .* cos(data.Omega));
 z = data.R .* (sin(n.*t + data.phi) .* sin(data.i));
 
-% loc = table(...
-%     ... % x
-%     data.R(id) .* (cos(n(id)*t + data.phi(id)) .* cos(data.Omega(id)) - ...
-%     sin(n(id)*t + data.phi(id)) .* cos(data.i(id)) .* sin(data.Omega(id))), ...
-%     ... % y
-%     data.R(id) .* (cos(n(id)*t + data.phi(id)) .* sin(data.Omega(id)) + ...
-%     sin(n(id)*t + data.phi(id)) .* cos(data.i(id)) .* cos(data.Omega(id))), ...
-%     'VariableNames', {'x1', 'y1'});
-
 %% Plot
 figure(1)
 clf
@@ -73,8 +70,9 @@ xlim([-40 40])
 ylim([-40 40])
 hold on
 
-% F(100) = struct('cdata',[],'colormap',[]);
-num_stars = length(n); % Set the number of stars to plot
+if REC_VIDEO; F(100) = struct('cdata',[],'colormap',[]); end %#ok<UNRCH>
+
+num_stars = MAX_STARS; % Set the number of stars to plot
 for i = 1:100
     if i == 1
         ax_star = plot3(x(1:num_stars, i), y(1:num_stars, i), ...
@@ -82,7 +80,7 @@ for i = 1:100
                         'Color', 'white'); % [0.3010, 0.7450, 0.9330]
         ax_ss = plot3(ss_x(i), ss_y(i), ss_z(i), 'r.', 'MarkerSize',30);
         h = gca;
-        set(h, 'Color', [0.30, 0.30, 0.30], 'XColor', [1 1 1], ...
+        set(h, 'Color', [0.10, 0.10, 0.10], 'XColor', [1 1 1], ...
             'YColor', [1 1 1]);
         set(gcf, 'Color', 'k');
         axis equal
@@ -98,27 +96,29 @@ for i = 1:100
         rotate3d on
 %         view(60,20);
     end
-    pause(0.1)
+    
+    if ~REC_VIDEO; pause(0.1); end 
+    
     ax_star.XData = x(1:num_stars, i);
     ax_star.YData = y(1:num_stars, i);
     ax_star.ZData = z(1:num_stars, i);
     ax_ss.XData = ss_x(i);
     ax_ss.YData = ss_y(i);
     ax_ss.ZData = ss_z(i);
-%     F(i) = getframe(gcf);
+    
+    if REC_VIDEO; F(i) = getframe(gcf); end %#ok<UNRCH>
 end
 
-
- 
  % defined in instructions
  function vc = orbit_speed(r)
      k = [...
-         0.00287729,   0.0023821,  -0.0010625,   0.000198502, ...
-         -1.88428e-05, 9.70521e-07, -2.70559e-08, 3.7516e-10, -1.94316e-12
+          0.00287729,   0.0023821,   -0.0010625, ...
+          0.000198502, -1.88428e-05, 9.70521e-07, ...
+          -2.70559e-08, 3.7516e-10,  -1.94316e-12
          ];
      vc = 1 ...
-          ./ (k(9)*r.^8 + k(8)*r.^7 + k(7)*r.^6 + k(6)*r.^5 ...
-             + k(5)*r.^4 + k(4)*r.^3 + k(3)*r.^2 + k(2)*r.^1 + k(1));
+          ./ (k(9)*r.^8 + k(8)*r.^7 + k(7)*r.^6 + k(6)*r.^5 + ...
+          k(5)*r.^4 + k(4)*r.^3 + k(3)*r.^2 + k(2)*r.^1 + k(1));
  end
  
  % defined in instructions
